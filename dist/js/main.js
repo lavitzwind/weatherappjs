6e6aacd6572b83a8462f6fb528cc3f143eee1d67
milleseseau <leojimlavi@gmail.com>
2021-05-22 07:24:24 -0500

    commit javascript

---
+++ dist/js/main.js
@@ -0,0 +1,41 @@
+import { setLocationObject } from "./dataFunctions.js";
+import { addSpinner, displayError } from "./domFunctions.js";
+import CurrentLocation from "./CurrentLocation.js";
+const currentLoc = new CurrentLocation();
+
+const initApp = () => {
+    // add listeners
+    const geoButton = document.getElementById("getLocation");
+    geoButton.addEventListener("click", getGeoWeather);
+    // set up
+
+    // load weather
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
+
+const getGeoWeather = (event) => {
+    if (event) {
+        if (event.type === "click") {
+            const mapIcon = document.querySelector(".fa-map-marker-alt");
+            addSpinner(mapIcon);
+        };
+    };
+    if (!navigator.geolocation) geoError();
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+};
+
+const geoError = (errObj) => {
+    const errMsg = errObj ? errObj.message : "Geolocation not supported";
+    displayError(errMsg, errMsg);
+};
+
+const geoSuccess = (position) => {
+    const myCoordsObj = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+        name : `Lat:${position.coords.latitude} Lon:${position.coords.longitude}`
+    };
+    setLocationObject(currentLoc, myCoordsObj);
+    console.log(currentLoc);
+};
