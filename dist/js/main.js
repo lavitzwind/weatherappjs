a41d32f21414d257438aab02b900bd9e6913a466
milleseseau <leojimlavi@gmail.com>
2021-05-24 07:29:36 -0500

    commit js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,13 @@
-import { setLocationObject, getHomeLocation } from "./dataFunctions.js";
 import {
+	setLocationObject,
+	getHomeLocation,
+	cleanText,
+} from "./dataFunctions.js";
+import {
+	setPlaceholderText,
 	addSpinner,
 	displayError,
+	displayApiError,
 	updateScreenReaderConfirmation,
 } from "./domFunctions.js";
 import CurrentLocation from "./CurrentLocation.js";
@@ -15,8 +21,14 @@ const initApp = () => {
 	homeButton.addEventListener("click", loadWeather);
 	const saveButton = document.getElementById("saveLocation");
 	saveButton.addEventListener("click", saveLocation);
+	const unitButton = document.getElementById("unit");
+	unitButton.addEventListener("click", setUnitPref);
+	const refreshButton = document.getElementById("refresh");
+	refreshButton.addEventListener("click", refreshWeather);
+	const locationEntry = document.getElementById("searchBar__form");
+	locationEntry.addEventListener("submit", submitNewLocation);
 	// set up
-
+	setPlaceholderText();
 	// load weather
 	loadWeather();
 };
@@ -94,6 +106,37 @@ const saveLocation = () => {
 		updateScreenReaderConfirmation(
 			`Saved ${currentLoc.getName()} as home location.`
 		);
+	}
+};
+
+const setUnitPref = () => {
+	const unitIcon = document.querySelector(".fa-chart-bar");
+	addSpinner(unitIcon);
+	currentLoc.toggleUnit();
+	updateDataAndDisplay(currentLoc);
+};
+
+const refreshWeather = () => {
+	const refreshIcon = document.querySelector(".fa-sync-alt");
+	addSpinner(refreshIcon);
+	updateDataAndDisplay(currentLoc);
+};
+
+const submitNewLocation = async (event) => {
+	event.preventDefault();
+	const text = document.getElementById("searchBar__text").value;
+	const entryText = cleanText(text);
+	if (!entryText.length) return;
+	const locationIcon = document.querySelector(".fa-search");
+	addSpinner(locationIcon);
+	const coordsData = await getCoordsFromApi(entryText, currentLoc.getUnit());
+	if (coordsData.cod === 200) {
+		// work with api data
+		const myCoordsObj = {};
+		setLocationObject(currentLoc, myCoordsObj);
+		updateDataAndDisplay(currentLoc);
+	} else {
+		displayApiError(coordsData);
 	}
 };
 
