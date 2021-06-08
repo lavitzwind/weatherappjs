b4fd833a40b2f9d4c89ea3f099c605f503d89396
milleseseau <leojimlavi@gmail.com>
2021-06-08 07:02:22 -0500

    general fix

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -18,11 +18,11 @@ export const getWeatherFromCoords = async (locationObj) => {
 	const units = locationObj.getUnit();
 	const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
 	try {
-	  const weatherStream = await fetch(url);
-	  const weatherJson = await weatherStream.json();
-	  return weatherJson;
+	    const weatherStream = await fetch(url);
+	    const weatherJson = await weatherStream.json();
+	    return weatherJson;
 	} catch (err) {
-	  console.error(err);
+	    console.error(err);
 	} */
 
 	const urlDataObj = {
@@ -48,11 +48,11 @@ export const getCoordsFromApi = async (entryText, units) => {
 	const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
 	const encodedUrl = encodeURI(url);
 	try {
-	  const dataStream = await fetch(encodedUrl);
-	  const jsonData = await dataStream.json();
-	  return jsonData;
+	    const dataStream = await fetch(encodedUrl);
+	    const jsonData = await dataStream.json();
+	    return jsonData;
 	} catch (err) {
-	  console.error(err.stack);
+	    console.error(err.stack);
 	} */
 
 	const urlDataObj = {
